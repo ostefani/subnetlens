3cc117146f9bfcc63aecb7de4285d40e8bc33d2a
ostefani <ostefanishina@gmail.com>
2026-03-29 00:23:59 -0600

    fix: use golang.org/x/sys/unix for SO_REUSEPORT in mDNS socket

--- scanner/mdns_socket_unix.go
+++ scanner/mdns_socket_unix.go
@@ -6,6 +6,8 @@ import (
 	"context"
 	"net"
 	"syscall"
+
+	"golang.org/x/sys/unix"
 )
 
 func newMDNSSocket() (net.PacketConn, error) {
@@ -13,8 +15,8 @@ func newMDNSSocket() (net.PacketConn, error) {
 		Control: func(network, address string, c syscall.RawConn) error {
 			var setSockErr error
 			err := c.Control(func(fd uintptr) {
-				setSockErr = syscall.SetsockoptInt(
-					int(fd), syscall.SOL_SOCKET, syscall.SO_REUSEPORT, 1,
+				setSockErr = unix.SetsockoptInt(
+					int(fd), unix.SOL_SOCKET, unix.SO_REUSEPORT, 1,
 				)
 			})
 			if err != nil {
